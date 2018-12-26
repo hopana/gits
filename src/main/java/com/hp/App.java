c5146013eb4cbba8b80a67efce0f820f703c4a4f
hopana <hopana@163.com>
2018-12-26 11:35:14 +0800

    从branch1合并test方法到App类

--- src/main/java/com/hp/App.java
+++ src/main/java/com/hp/App.java
@@ -2,12 +2,13 @@ package com.hp;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public void test() {
+        System.out.println("这是一个测试类");
     }
 }
