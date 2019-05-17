5b500c89f7515c3d677c80245e56dd7ba4cd5b88
hopana <hopana@163.com>
2019-05-17 14:38:02 +0800

    新增吃美食方法

--- src/main/java/com/hp/Test.java
+++ src/main/java/com/hp/Test.java
@@ -1,11 +1,15 @@
 package com.hp;
 
 /**
- * TODO 注释
+ * Test类
  *
  * @author hupan
  * @date 2019/01/02
  */
 public class Test {
+
+    public void eat() {
+        System.out.println("eat food...");
+    }
 
 }
