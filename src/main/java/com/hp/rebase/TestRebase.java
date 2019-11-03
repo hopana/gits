79ba949cc55e4ad31aad0b1848e65ea0bd50090e
hopana <hopana@163.com>
2019-11-03 15:57:58 +0800

    m:增加Rebase测试类

---
+++ src/main/java/com/hp/rebase/TestRebase.java
@@ -0,0 +1,15 @@
+package com.hp.rebase;
+
+/**
+ * TODO 注释
+ *
+ * @author hupan
+ * @date 2019/11/03
+ */
+public class TestRebase {
+
+    public void testRebase() {
+        System.out.println("add testRebase function by master");
+    }
+
+}
