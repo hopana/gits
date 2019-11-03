eb06631e8326d252c090c4338f253504886a1a64
hopana <hopana@163.com>
2019-11-03 17:15:13 +0800

    f:添加特性3 + 修复特性1

--- src/main/java/com/hp/cherry_pick/Feature1.java
+++ src/main/java/com/hp/cherry_pick/Feature1.java
@@ -9,6 +9,7 @@ package com.hp.cherry_pick;
 public class Feature1 {
 
     public void feature1() {
+        System.out.println("do some fixup!");
         System.out.println("feature branch add feature 1");
     }
 
---
+++ src/main/java/com/hp/cherry_pick/Feature3.java
@@ -0,0 +1,15 @@
+package com.hp.cherry_pick;
+
+/**
+ * 特性3
+ *
+ * @author hupan
+ * @date 2019/11/03
+ */
+public class Feature3 {
+
+    public void feature3() {
+        System.out.println("feature branch add feature 3");
+    }
+
+}
