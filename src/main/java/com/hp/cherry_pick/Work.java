a09f7d54f9670e2dc37999034da047ff3a2ec676
hopana <hopana@163.com>
2019-11-03 16:56:39 +0800

    d:主任务开发中...

---
+++ src/main/java/com/hp/cherry_pick/Work.java
@@ -0,0 +1,15 @@
+package com.hp.cherry_pick;
+
+/**
+ * dev：主任务开发
+ *
+ * @author hupan
+ * @date 2019/11/03
+ */
+public class Work {
+
+    public void doWork() {
+        System.out.println("dev branch do some work...");
+    }
+
+}
