80bba691d607870e91865b234afbfe09908095ee
hopana <hopana@163.com>
2019-11-03 16:49:33 +0800

    f:添加特性2

---
+++ src/main/java/com/hp/cherry_pick/Feature2.java
@@ -0,0 +1,15 @@
+package com.hp.cherry_pick;
+
+/**
+ * 特性2
+ *
+ * @author hupan
+ * @date 2019/11/03
+ */
+public class Feature2 {
+
+    public void feature2() {
+        System.out.println("feature branch add feature 2");
+    }
+
+}
