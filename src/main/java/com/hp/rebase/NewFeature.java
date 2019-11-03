92a8deeea8d5193b9478b0a45ed316d51b0a01a4
hopana <hopana@163.com>
2019-11-03 16:02:13 +0800

    f:添加新特性类

---
+++ src/main/java/com/hp/rebase/NewFeature.java
@@ -0,0 +1,15 @@
+package com.hp.rebase;
+
+/**
+ * 新特性
+ *
+ * @author hupan
+ * @date 2019/11/03
+ */
+public class NewFeature {
+
+    public void addNewFeature() {
+        System.out.println("add newFeature by feature branch");
+    }
+
+}
