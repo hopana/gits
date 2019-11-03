b6fb7c30db9e84e27dba1945a54198a9c4230e49
hopana <hopana@163.com>
2019-11-03 16:09:33 +0800

    d:添加主任务Work类

---
+++ src/main/java/com/hp/rebase/Work.java
@@ -0,0 +1,15 @@
+package com.hp.rebase;
+
+/**
+ * dev：主任务开发
+ *
+ * @author hupan
+ * @date 2019/11/03
+ */
+public class Work {
+
+    public void remark() {
+        System.out.println("this is the mian work add by dev branch");
+    }
+
+}
