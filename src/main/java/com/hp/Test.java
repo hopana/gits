d8f9c1c71d86dd6f78dfa4413218ac5f2b7717e8
hopana <hopana@163.com>
2020-01-16 17:23:11 +0800

    master: 添加Test类并添加sleep和hike方法

---
+++ src/main/java/com/hp/Test.java
@@ -0,0 +1,19 @@
+package com.hp;
+
+/**
+ * Test类
+ *
+ * @author hupan
+ * @date 2020/01/16
+ */
+public class Test {
+
+    public void sleep() {
+        System.out.println("sleep at night...");
+    }
+
+    public void hike() {
+        System.out.println("hike at mountain...");
+    }
+
+}
