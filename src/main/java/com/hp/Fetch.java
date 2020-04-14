ae7ad4349f3d57de9445ebadc5b3e3a4d95746c2
hupan <hopana@163.com>
2020-04-14 16:57:51 +0800

    branch2: 新增Fetch类、Test类、gitignore

---
+++ src/main/java/com/hp/Fetch.java
@@ -0,0 +1,15 @@
+package com.hp;
+
+/**
+ * 演示Fetch使用
+ *
+ * @author hupan
+ * @date 2020-04-14 16:52
+ */
+public class Fetch {
+
+    public void func() {
+        System.out.println("Class for Fetch demo...");
+    }
+
+}
---
+++ src/main/java/com/hp/Test.java
@@ -0,0 +1,15 @@
+package com.hp;
+
+/**
+ * 测试类
+ *
+ * @author hupan
+ * @date 2020-04-14 16:56
+ */
+public class Test {
+
+    public void test() {
+        System.out.println("nothing here....");
+    }
+
+}
