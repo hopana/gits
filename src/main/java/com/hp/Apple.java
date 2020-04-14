5ab501c9927a2aeed3f8b7f39e512741b8717aee
hopana <hopana@163.com>
2020-04-14 20:20:38 +0800

    branch1: 新增Apple类

---
+++ src/main/java/com/hp/Apple.java
@@ -0,0 +1,16 @@
+package com.hp;
+
+/**
+ * Apple
+ *
+ * @author hupan
+ * @date 2020/04/14
+ */
+public class Apple {
+
+    // to test merge with no conflicts
+    public void iphone4s() {
+        System.out.println("iphone 4s is the best phone in the world!");
+    }
+
+}
